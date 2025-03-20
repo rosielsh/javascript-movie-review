7a62416af575faba73a3ffad8ff499c4c8b4518a
SuHwa Lee <tnghk9611@naver.com>
2025-03-20 17:13:01 +0900

    Updates

--- assets/index-CfLoGvRb.js
+++ assets/index-CfLoGvRb.js
@@ -125,7 +125,7 @@ const STATUS_MESSAGE = {
 class HttpError extends Error {
   constructor(status) {
     if (!Object.hasOwnProperty.call(STATUS_MESSAGE, status)) {
-      throw new Error(`Invalid status code: ${status}`);
+      throw new Error(`에러가 발생했어요. ${status}`);
     }
     super(STATUS_MESSAGE[status]);
     __publicField(this, "status");
@@ -303,7 +303,7 @@ const IconButton = ({
 };
 const SearchBar = ({ onSubmit, classList, props }) => {
   const input = createElement("input", {
-    id: "search-input",
+    classList: "search-input",
     type: "text",
     placeholder: "검색어를 입력해주세요."
   });
@@ -317,7 +317,7 @@ const SearchBar = ({ onSubmit, classList, props }) => {
   });
   const formElement = createElement("form", {
     ...props,
-    classList: "w-525 h-36 flex items-center justify-between rounded-2xl border-2 px6-py16",
+    classList: "form-container",
     children: [input, iconBtn]
   });
   formElement.addEventListener("submit", (event) => {
@@ -328,7 +328,7 @@ const SearchBar = ({ onSubmit, classList, props }) => {
 };
 const createHeaderSection = () => {
   return createElement("section", {
-    classList: "flex flex-row items-center gap-250",
+    classList: "section-container",
     children: [
       IconButton({
         width: "117",
@@ -421,7 +421,7 @@ const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w220_and_h330_face";
 const DEFAULT_IMAGE_URL = "./images/no_image.png";
 const createRatingSection = (vote_average) => {
   return Box({
-    classList: ["flex", "gap-8"],
+    classList: ["movie-rate"],
     props: {
       children: [
         Img({
@@ -441,7 +441,7 @@ const createRatingSection = (vote_average) => {
 };
 const createDescriptionSection = (title, vote_average) => {
   return Box({
-    classList: ["flex", "flex-col", "gap-10"],
+    classList: ["movie-description"],
     props: {
       children: [
         createRatingSection(vote_average),
@@ -470,7 +470,7 @@ const MovieItem = ({
   poster_path
 }) => {
   return createElement("li", {
-    classList: "movie-item flex flex-col justify-start gap-10",
+    classList: "movie-item",
     children: [
       createMovieImage(title, poster_path),
       createDescriptionSection(title, vote_average)
